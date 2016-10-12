aa4fd4aa5361fd053bfa1b051a38244d1c32bb21
Pablo Gutiérrez Díaz <dxpablo2012@hotmail.com>
2016-10-12 18:49:05 -0500

    Ejemplo de google maps
    Ejemplo de google maps.

---
+++ EjemploGoogleMaps/app/src/main/java/com/pablo/pgutierrezd/ejemplogooglemaps/MainActivity.java
@@ -0,0 +1,126 @@
+package com.pablo.pgutierrezd.ejemplogooglemaps;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMarkerDragListener{
+
+    private Marker markerPais;
+    private FirstMapFragment mFirstMapFragment;
+    private static final int LOCATION_REQUEST_CODE = 1;
+    private GoogleMap mMap;
+    private EditText ed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed = (EditText) findViewById(R.id.txtCampo);
+
+        mFirstMapFragment = FirstMapFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.map_container, mFirstMapFragment)
+                .commit();
+        // Registrar escucha onMapReadyCallback
+        mFirstMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        googleMap.moveCamera(CameraUpdateFactory.zoomBy(100));
+
+        LatLng queretaro = new LatLng(20.5872194, -100.387161);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(queretaro, 15));
+        googleMap.moveCamera(CameraUpdateFactory.);
+
+        // Controles UI
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Mostrar diálogo explicativo
+            } else {
+                // Solicitar permiso
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            }
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Marcadores
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+
+        //Events
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            // ¿Permisos asignados?
+            if (permissions.length > 0 &&
+                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Error de permisos", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true));
+        ed.setText("Latitud: " + latLng.latitude + " Longitud: " + latLng.longitude);
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        ed.setText("Latitud: "+marker.getPosition().latitude +" Longitud: "+marker.getPosition().longitude);
+    }
+}
